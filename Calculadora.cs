c7c1729f78a1ff035ff31031f30d71806d2bb786
Molusco05 <moniccfssantos@gmail.com>
2026-02-26 21:11:55 -0300

    commit codigo completo

---
+++ Calculadora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerceiroProjeto
+{
+    public class Calculadora
+    {
+        public decimal CalcularDesconto(decimal preco)
+        {
+            decimal desconto = preco * 0.10m;
+            return preco - desconto;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ namespace TerceiroProjeto
     {
         static void Main(string[] args)
         {
+            decimal precoProduto = 500.00m;
+
+            Console.WriteLine("Preço original do produto: R$ " + precoProduto);
+
+            Calculadora calc = new Calculadora();
+
+            precoProduto = calc.CalcularDesconto(precoProduto);
+
+            Console.WriteLine("Valor com 10% de desconto: R$ " + precoProduto);
+
+            Console.ReadKey();
         }
     }
 }
